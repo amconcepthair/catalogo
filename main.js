7ad63d3706134ac2e857d9cf5ef3ee75f3e65c12
Matheus Jurkovich <77711064+matheusjurkovich@users.noreply.github.com>
2022-07-23 13:29:38 -0300

    Added aesthetics article and buttons

--- main.js
+++ main.js
@@ -5,3 +5,23 @@ function openMenu() {
 function closeMenu() {
     document.body.classList.remove('menu-expanded')
 }
+
+//Função para alterar valores do article
+const btnAndreia = document.querySelector('.btn-andreia')
+const divAndreia = document.querySelector('#aesthetics-andreia')
+const btnCyntia = document.querySelector('.btn-cyntia')
+const divCyntia = document.querySelector('#aesthetics-cyntia')
+
+btnAndreia.addEventListener('click', () => {
+    if(divAndreia.classList.contains('hidden')) {
+        divAndreia.classList.remove('hidden')
+        divCyntia.classList.add('hidden')
+    }
+})
+
+btnCyntia.addEventListener('click', () => {
+    if(divCyntia.classList.contains('hidden')) {
+        divCyntia.classList.remove('hidden')
+        divAndreia.classList.add('hidden')
+    }
+})
